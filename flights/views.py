ef44d1cfe5429031882862ecbdaeec45b91a3a69
Hessah95 <hessah.alhamadi.95@gmail.com>
2019-09-02 23:12:56 +0300

    Hessah & Amnah

--- flights/views.py
+++ flights/views.py
@@ -1,1 +1,14 @@
 from .models import Flight, Booking
+from rest_framework.generics import ListAPIView
+from .serializers import FlightSerializers, BookingSerializers
+from django.utils import timezone
+
+
+class Flights (ListAPIView) :
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializers
+
+
+class Bookings (ListAPIView) :
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializers
